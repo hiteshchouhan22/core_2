08f18ff023047492a3f14447655f2fedb5ce5497
HITESH\hitesh <24716760+hiteshchouhan22@users.noreply.github.com>
2023-07-08 04:30:53 +0530

    init

---
+++ components/Connected.tsx
@@ -0,0 +1,157 @@
+import { FC, MouseEventHandler, useCallback, useEffect, useMemo, useState } from "react"
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+  Image,
+  Center,
+} from "@chakra-ui/react"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
+import { useRouter } from "next/router"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { CandyMachine, CandyMachineV2, Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js"
+import { PublicKey } from "@solana/web3.js"
+
+interface MyNftInterface {
+  name: string;
+  image: string;
+}
+
+const Connected: FC = () => {
+  const router = useRouter()
+  const walletAdapter = useWallet();
+  const { connection } = useConnection();
+  const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter))
+  const [isMinting, setIsMinting] = useState(false)
+  const [candyMachine, setCandyMachine] = useState<CandyMachineV2>()
+  const [nftData, setNftData] = useState<MyNftInterface[]>()
+
+  const fetchNfts = async () => {
+    if (!walletAdapter.connected || !walletAdapter.publicKey) {
+      return;
+    }
+    
+    const cm = await metaplex.candyMachinesV2().findByAddress({address: new PublicKey("Dzzi9AfyFAXPL6KDRrcNFEDyq1i9uaGAhXhuCnmxnQEG")})
+
+    if (!cm) {
+      return;
+    }
+    setCandyMachine(cm);
+
+    console.log(candyMachine)
+    // fetch off chain metadata for each NFT
+    let nftData = []
+    for (let i = 0; i < cm.items.length; i++) {
+      let fetchResult = await fetch(cm.items[i].uri)
+      let json = await fetchResult.json()
+
+      if (json.symbol == "GALX") {
+        nftData.push(json)
+        console.log(json)
+      }
+    }
+
+    // set state
+    setNftData(nftData)
+  }
+  
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {
+      const candyMachine = await metaplex.candyMachinesV2().findByAddress({address: new PublicKey("Dzzi9AfyFAXPL6KDRrcNFEDyq1i9uaGAhXhuCnmxnQEG")});
+
+      console.log(candyMachine);
+      
+      if (event.defaultPrevented) return;
+      if (!walletAdapter.connected || !candyMachine) return;
+  
+      try {
+        setIsMinting(true);
+        const nft = await metaplex.candyMachinesV2().mint({ candyMachine });        
+  
+        router.push(`/?mint=${nft.nft.address.toBase58()}`);
+      } catch (error) {
+        console.log(error);
+        alert(error);
+      } finally {
+        setIsMinting(false);
+      }
+    },
+    [metaplex, walletAdapter, router]
+  );  
+
+  const handleStakeClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    async (event) => {
+
+      alert('Not implemented')
+    },
+    [walletAdapter, candyMachine]
+  );  
+
+  useEffect(() => {
+    fetchNfts()
+  }, [walletAdapter, connection])
+
+  return (
+    <VStack spacing={20}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            Dear PASS maker
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            Each GLX is randomly generated and can be staked to receive
+            <Text as="b"> $PASH</Text> Use your <Text as="b"> $PASH</Text> to
+            upgrade your PASH and receive perks within the community!
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={10}>
+        {nftData && nftData.map((nft) => (
+          <Image key={nft.name} src={nft.image} alt={nft.name} width="100" height="100" />
+        ))}
+      </HStack>
+
+      <Button
+          bgColor="accent"
+          color="white"
+          maxWidth="380px"
+          onClick={handleClick}
+          disabled={isMinting}
+        >
+          <HStack>
+            <Text>{!isMinting ? 'mint pash' : 'wait a bit...'}</Text>
+            <ArrowForwardIcon />
+          </HStack>
+      </Button>
+
+      <Center>
+        <Button
+          bgColor="accent"
+          color="white"
+          maxWidth="380px"
+          onClick={handleStakeClick}
+        >
+          <HStack>
+            <Text>stake my PASH</Text>
+            <ArrowForwardIcon />
+          </HStack>
+        </Button>
+      </Center>
+
+
+    </VStack>
+  )
+}
+
+export default Connected
---
+++ components/NewMint.tsx
@@ -0,0 +1,91 @@
+import { FC, MouseEventHandler, useCallback, useEffect, useMemo, useState } from "react"
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+  Image,
+} from "@chakra-ui/react"
+import { ArrowForwardIcon } from "@chakra-ui/icons"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useRouter } from "next/router"
+import { Metaplex, Nft } from "@metaplex-foundation/js"
+import { PublicKey } from "@solana/web3.js"
+
+interface MyNft {
+  name: string;
+  image: string;
+}
+
+const NewMint: FC = () => {
+  const { connection } = useConnection()
+  const walletAdapter = useWallet()
+  const router = useRouter()
+
+  const metaplex = Metaplex.make(connection)
+
+  const [mintedInfo, setMintedInfo] = useState<MyNft>();
+  const fetchNft = async () => {
+    const mintedAddress = router.query.mint ?? null;
+    if (mintedAddress == null) {
+      return
+    }
+  
+    const nftPayload = await metaplex.nfts().findByMint({ 
+      mintAddress:   new PublicKey(mintedAddress)
+    })
+
+    let fetchResult = await fetch(nftPayload.uri)
+    let json = await fetchResult.json()
+
+    console.log(json);
+
+    if (json.symbol == "PASH") {
+      setMintedInfo(json)
+    }
+  }
+
+  useEffect(() => {
+    fetchNft()
+  }, [walletAdapter])
+
+  return (
+    <VStack spacing={20}>
+      <Container>
+        <VStack spacing={8}>
+          <Heading
+            color="white"
+            as="h1"
+            size="2xl"
+            noOfLines={1}
+            textAlign="center"
+          >
+            A new PASS NFT appeared just for you!! 
+          </Heading>
+
+          <Text color="bodyText" fontSize="xl" textAlign="center">
+            How lucky you are to have collected
+            <Text as="b"> $PASH</Text> Use your <Text as="b"> $PASH</Text> to
+            enjoy NFTs reserved for the private club
+          
+          </Text>
+        </VStack>
+      </Container>
+
+      <HStack spacing={10}>
+        {mintedInfo && mintedInfo.image ? <Image src={mintedInfo.image} alt="" /> : ''}
+      </HStack>
+
+      <Button bgColor="accent" color="white" maxW="380px">
+        <HStack>
+          <Text>mint buildoor</Text>
+          <ArrowForwardIcon />
+        </HStack>
+      </Button>
+    </VStack>
+  )
+}
+
+export default NewMint
